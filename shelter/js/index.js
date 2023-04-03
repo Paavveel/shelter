1ddd6c543d0602aad6ebab34f90d880b8927ee81
Paavveel <pa.tomilin@gmail.com>
2023-04-03 18:52:49 +0300

    feat: add mobile menu logic

--- shelter/js/index.js
+++ shelter/js/index.js
@@ -1,3 +1,56 @@
-console.log(`Все пункты выполнены согласно ТЗ
-Оценка - 100/100
-`);
+const menuOverlay = document.querySelector('.menu__overlay');
+const menuBody = document.querySelector('.menu__body');
+const menuButton = document.querySelector('.menu__button');
+
+function closeMenu() {
+  menuOverlay.classList.remove('overlay_show');
+  menuBody.classList.remove('menu__body_show');
+  menuButton.classList.remove('menu__button_open');
+  document.body.classList.remove('body_lock');
+}
+
+if (menuBody && menuButton) {
+  menuButton.addEventListener('click', () => {
+    menuOverlay.classList.toggle('overlay_show');
+    menuBody.classList.toggle('menu__body_show');
+    menuButton.classList.toggle('menu__button_open');
+    document.body.classList.toggle('body_lock');
+  });
+
+  menuBody.addEventListener('click', (e) => {
+    if (
+      e.target.matches('.menu__link') &&
+      menuBody.matches('.menu__body_show')
+    ) {
+      e.preventDefault();
+
+      const link = e.target;
+      const href = link.getAttribute('href');
+
+      closeMenu();
+
+      if (href.includes('.html')) {
+        const url = location.href.split('/');
+        url[url.length - 1] = href;
+        const newUrl = url.join('/');
+
+        setTimeout(() => {
+          location.assign(newUrl);
+        }, 330);
+      } else {
+        const blockId = href.replace('#', '');
+        const block = document.getElementById(blockId);
+
+        setTimeout(() => {
+          block.scrollIntoView({
+            behavior: 'smooth',
+            block: 'start',
+          });
+          location.hash = blockId;
+        }, 330);
+      }
+    }
+  });
+
+  menuOverlay.addEventListener('click', closeMenu);
+}
