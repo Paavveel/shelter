dfc7ecdec1f4341cc063a2c0d509b9637b50cafa
Paavveel <pa.tomilin@gmail.com>
2023-04-08 12:36:03 +0300

    fix: remove/add menu listeners

--- shelter/js/index.js
+++ shelter/js/index.js
@@ -5,61 +5,62 @@ const menuOverlay = document.querySelector('.menu__overlay');
 const menuBody = document.querySelector('.menu__body');
 const menuButton = document.querySelector('.menu__button');
 
-function closeMenu() {
-  menuOverlay.classList.remove('overlay_show');
-  menuBody.classList.remove('menu__body_show');
-  menuButton.classList.remove('menu__button_open');
-  document.body.classList.remove('body_lock');
+function toggleMenu() {
+  menuOverlay.classList.toggle('overlay_show');
+  menuBody.classList.toggle('menu__body_show');
+  menuButton.classList.toggle('menu__button_open');
+  document.body.classList.toggle('body_lock');
+
+  if (menuBody.matches('.menu__body_show')) {
+    menuBody.addEventListener('click', handleMenuLink);
+    menuOverlay.addEventListener('click', toggleMenu);
+  } else {
+    menuBody.removeEventListener('click', handleMenuLink);
+    menuOverlay.removeEventListener('click', toggleMenu);
+  }
+}
+
+function handleMenuLink(e) {
+  const link = e.target;
+
+  if (link.matches('.menu__link') && menuBody.matches('.menu__body_show')) {
+    e.preventDefault();
+
+    const href = link.getAttribute('href');
+
+    if (href === '#' || link.matches('.menu__link_active')) {
+      toggleMenu();
+      location.hash = '';
+      return;
+    }
+
+    toggleMenu();
+
+    if (href.includes('.html')) {
+      const url = location.href.split('/');
+      url[url.length - 1] = href;
+      const newUrl = url.join('/');
+
+      setTimeout(() => {
+        location.assign(newUrl);
+      }, 330);
+    } else {
+      const blockId = href.replace('#', '');
+      const block = document.getElementById(blockId);
+
+      setTimeout(() => {
+        block.scrollIntoView({
+          behavior: 'smooth',
+          block: 'start',
+        });
+        location.hash = blockId;
+      }, 330);
+    }
+  }
 }
 
 if (menuBody && menuButton) {
-  menuButton.addEventListener('click', () => {
-    menuOverlay.classList.toggle('overlay_show');
-    menuBody.classList.toggle('menu__body_show');
-    menuButton.classList.toggle('menu__button_open');
-    document.body.classList.toggle('body_lock');
-  });
-
-  menuBody.addEventListener('click', (e) => {
-    const link = e.target;
-
-    if (link.matches('.menu__link') && menuBody.matches('.menu__body_show')) {
-      e.preventDefault();
-
-      const href = link.getAttribute('href');
-
-      if (href === '#' || link.matches('.menu__link_active')) {
-        closeMenu();
-        location.hash = '';
-        return;
-      }
-
-      closeMenu();
-
-      if (href.includes('.html')) {
-        const url = location.href.split('/');
-        url[url.length - 1] = href;
-        const newUrl = url.join('/');
-
-        setTimeout(() => {
-          location.assign(newUrl);
-        }, 330);
-      } else {
-        const blockId = href.replace('#', '');
-        const block = document.getElementById(blockId);
-
-        setTimeout(() => {
-          block.scrollIntoView({
-            behavior: 'smooth',
-            block: 'start',
-          });
-          location.hash = blockId;
-        }, 330);
-      }
-    }
-  });
-
-  menuOverlay.addEventListener('click', closeMenu);
+  menuButton.addEventListener('click', toggleMenu);
 }
 
 // Menu END
